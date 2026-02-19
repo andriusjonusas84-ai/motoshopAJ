17ca1e4ae97023d0bf97e34f85f7fa5a5e4c114b
Andrius Jonusas <andrius.jonusas84@gmail.com>
2026-02-19 13:13:18 +0200

    Sukurtas appsas ir sukurtas CustomUser modelis

---
+++ mysite/motoshop/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User, AbstractUser
+from PIL import Image
+
+
+class CustomUser(AbstractUser):
+    photo = models.ImageField(upload_to='profile_pics',
+                              null=True,
+                              blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.photo:
+            img = Image.open(self.photo.path)
+            min_side = min(img.width, img.height)
+            left = (img.width - min_side) // 2
+            top = (img.height - min_side) // 2
+            right = left + min_side
+            bottom = top + min_side
+            img = img.crop((left, top, right, bottom))
+            img = img.resize((300, 300), Image.LANCZOS)
+            img.save(self.photo.path)
